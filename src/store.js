4b25059845dd7cf6164ce693e781de1ead88bca5
wangpin34 <270280522@qq.com>
2016-06-12 20:36:30 +0800

    Refactor store

--- src/store.js
+++ src/store.js
@@ -1,42 +1,42 @@
-export const state = {
-	notes: {},
-	onEditNote: null,
-	editing: false
-}
+export default {
 
-export const actions = {
+	state: {
+		notes: {},
+		onEditNote: null,
+		editing: false
+	},
 
 	//Go to edit the select note
 	goEdit(id) {
-		state.editing = true
-		state.onEditNote = Object.assign({}, state.notes[id])
+		this.state.editing = true
+		this.state.onEditNote = Object.assign({}, this.state.notes[id])
 	},
 
 	//Go to add a new note
 	addNote() {
 		let note = addNote()
-		state.onEditNote = Object.assign({}, note)
-		state.editing = true		
+		this.state.onEditNote = Object.assign({}, note)
+		this.state.editing = true		
 	},
 
 	//Update the specify note
 	updateNote(note) {
 		note = Object.assign({}, note)
-		state.notes[note.id] = note
 		note.updatedAt = new Date()
-		state.editing = false
+		this.state.notes[note.id] = note
+		this.state.editing = false
 	},
 
 	//Cancel current edit, it shall be add note or update note
 	cancelEdit() {
-		state.editing = false
-		state.onEditNote = null
+		this.state.editing = false
+		this.state.onEditNote = null
 	},
 
 	//Fetch notes from local file system
 	fetchNotes(notes) {
 		//let notes = []
-		state.notes = Object.assign(state.notes, notes)
+		this.state.notes = Object.assign(this.state.notes, notes)
 	}
 
 }
